3bb350d61953bcee2e9c5d672811a9f872ecb8f8
Prosukov Oleksandr <122283728+ProsukovOleksandr@users.noreply.github.com>
2023-06-11 18:47:39 +0300

    update goit-js-hw-10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,12 +1,10 @@
-/* RENAME THIS FILE TO cat-api.js*/
-
 export let fetchBreeds = () =>{
     const url = "https://api.thecatapi.com/v1/breeds";
      const catsPromise = fetch(url);
      return catsPromise;
 };
-export let fetchCatByBreed = (breedId)=> {
-const catUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key= !!!!PLACE YOUR API_KEY HERE!!!!`
+export let fetchCatByBreed = (breedId)=>{
+const catUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=live_WJpJKiHwowP5MuhrNrTMBLEd4AF86LdVlOt6vRgrwjrRmfVkcLin13gdEk0Tkirv`
 const catPromise = fetch(catUrl);
 return catPromise;
 }
