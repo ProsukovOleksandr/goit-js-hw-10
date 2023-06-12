b0c8dcdb214682518f656fe095ed9fe9ea4b23bc
Prosukov Oleksandr <122283728+ProsukovOleksandr@users.noreply.github.com>
2023-06-12 10:15:10 +0300

    update goit-js-hw-10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,10 +1,18 @@
+const loaderPar = document.querySelector(".loader");
+const errorPar = document.querySelector('.error');
+
 export let fetchBreeds = () =>{
     const url = "https://api.thecatapi.com/v1/breeds";
-     const catsPromise = fetch(url);
-     return catsPromise;
+    return fetch(url).then(response => response.json()).catch(()=>{
+        loaderPar[0].style.display = "none";
+        errorPar[0].style.display = "block";
+      });
 };
 export let fetchCatByBreed = (breedId)=>{
 const catUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=live_WJpJKiHwowP5MuhrNrTMBLEd4AF86LdVlOt6vRgrwjrRmfVkcLin13gdEk0Tkirv`
-const catPromise = fetch(catUrl);
-return catPromise;
+return fetch(catUrl).then(response => response.json()).catch(()=>{
+    loaderPar[0].style.display = "none";
+    errorPar[0].style.display = "block";
+  });
+
 }
--- src/index.js
+++ src/index.js
@@ -1,47 +1,41 @@
 import { fetchBreeds } from './cat-api.js';
 import { fetchCatByBreed } from './cat-api.js';
-const selectEl = document.getElementsByClassName('breed-select');
-const loaderPar = document.getElementsByClassName('loader');
-const errorPar = document.getElementsByClassName('error');
-const catInfoEl = document.getElementsByClassName('cat-info');
-errorPar[0].style.display = "none";
+const selectEl = document.querySelector('.breed-select');
+const loaderPar = document.querySelector(".loader");
+const errorPar = document.querySelector('.error');
+const catInfoEl = document.querySelector('.cat-info');
+errorPar.style.display = "none";
 function createOption(name, id) {
   let option = `<option value ='${id}'>${name}</option>`;
-  selectEl[0].insertAdjacentHTML('afterbegin', option);
+  return option;
 }
 function createCatInfo(catInfo, catImg) {
   let info = `<img src = "${catImg}" width = 350 height = 300 class = "cat-img"><\img> <div class="cat-info-wrapper"> <h1 class  = "cat-title">${catInfo.name}</h1> <p class = "cat-descr">${catInfo.description}</p></div>`;
-  catInfoEl[0].insertAdjacentHTML('afterbegin', info);
+  catInfoEl.insertAdjacentHTML('afterbegin', info);
 }
 
 fetchBreeds()
-  .then(response => response.json())
-  .then(headlines => {
-    headlines.sort((a,  b)=> b.name.localeCompare(a.name)).map(({ name, id }) => {
-      createOption(name, id);
-      loaderPar[0].style.display = "none";
+  .then(headlines => { 
+    let options = headlines.map(({ name, id }) => {
+     let option = createOption(name, id);
+      loaderPar.style.display = "none";
+      return option;
     });
-  }).catch(()=>{
-    loaderPar[0].style.display = "none";
-    errorPar[0].style.display = "block";
-  });
+    selectEl.innerHTML  = [...options];
+  })
 
-selectEl[0].addEventListener('input', event => {
+selectEl.addEventListener('input', event => {
   event.preventDefault();
-  loaderPar[0].style.display = "block";
-  const breedId = selectEl[0].value;
-  catInfoEl[0].innerHTML = " ";
+  loaderPar.style.display = "block";
+  const breedId = selectEl.value;
+  catInfoEl.innerHTML = " ";
   fetchCatByBreed(breedId)
-    .then(response => response.json())
     .then(headline => {
       if (breedId === headline[0].breeds[0].id) {
         let info = headline[0].breeds[0];
         let img = headline[0].url;
         createCatInfo(info, img);
-        loaderPar[0].style.display = "none";
+        loaderPar.style.display = "none";
       }
-    }).catch(()=>{
-      loaderPar[0].style.display = "none";
-      errorPar[0].style.display = "block";
-    });
+    })
 })
